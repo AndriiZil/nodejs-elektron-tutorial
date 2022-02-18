5c910f4433c3a461b529b34ad10a8d491b0446a1
Andrii Zilnyk <andrii.zilnyk@eleks.com>
2022-02-18 14:36:55 +0200

    added main to render communication

---
+++ 04_main_to_render/renderer.js
@@ -0,0 +1,9 @@
+const counter = document.getElementById('counter');
+
+window.electronAPI.handleCounter((event, value) => {
+    const oldValue = Number(counter.innerText);
+    const newValue = oldValue + value;
+    counter.innerText = newValue;
+    console.log(event);
+    event.reply('counter-value', newValue);
+});

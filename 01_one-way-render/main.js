e26fc9fb4b9cb39d64a8e4e98a20e413b7a136e9
Andrii Zilnyk <andrii.zilnyk@eleks.com>
2022-02-18 13:06:52 +0200

    added one way render

---
+++ 01_one-way-render/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    ipcMain.on('set-title', (event, title) => {
+        console.log('title', title);
+        const webContents = event.sender
+        const win = BrowserWindow.fromWebContents(webContents)
+        win.setTitle(title)
+    })
+
+    mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
---
+++ 01_one-way-render/renderer.js
@@ -0,0 +1,10 @@
+const setButton = document.getElementById('btn');
+const titleInput = document.getElementById('title');
+
+setButton.addEventListener('click', () => {
+    const title = titleInput.value;
+
+    console.log(window.electronAPI);
+
+    window.electronAPI.setTitle(title)
+});
